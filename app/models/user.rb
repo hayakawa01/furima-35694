57a48e4c9712aaf51188c0e88ecbcbea1b3afcfe
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-19 18:10:49 +0900

    レビューをもとに修正③

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,15 @@ class User < ApplicationRecord
   
   with_options presence: true do
     validates :nickname
-    validates :first_name,format:{ with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "は、全角での入力が必須です" }
-    validates :last_name,format:{ with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "は、全角での入力が必須です" }
-    validates :first_name_kana,format:{ with: /\A[ァ-ヶーー]+\z/, message: "は、全角カタカナでの入力が必須です" }
-    validates :last_name_kana,format:{ with: /\A[ァ-ヶーー]+\z/, message: "は、全角カタカナでの入力が必須です" }
     validates :birth_date
+    with_options format:{ with: /\A[ぁ-んァ-ヶー-龥々ー]+\z/, message: "は、全角での入力が必須です" } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format:{ with: /\A[ァ-ヶーー]+\z/, message: "は、全角カタカナでの入力が必須です" } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
   end
   validates :password,format:{with: /\A(?=.*[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "は、半角英数字混合での入力が必須です"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items
-  post 'user_registration' ,to:'users#create'
+  #post 'user_registration' ,to:'users#create'
 end
