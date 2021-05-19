58250a502485fa9476497151bedc15151dd9eb7b
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-19 14:30:53 +0900

    pry_railsの導入と単体モデルテストの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
   validates :birth_date, presence: true
+  PASSWORD_REGEX = /\A[a-z\d]+\z/i
+  validates_format_of :password,{with: PASSWORD_REGEX,message: "は、半角英数字混合での入力が必須です"}
+  FULL_NAME_REGEX = /\A[ぁ-んァ-ヶー-龥々ー]+\z/
+  validates_format_of :first_name,:last_name,:first_name_kana,:last_name_kana,{with: FULL_NAME_REGEX,message: "は、全角での入力が必須です"}
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,65 +1,145 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
   describe '新規登録/ユーザー情報' do
-    it 'nickname,email,password,passeord_confirmation,first_name,last_name,first_name_kana,last_name_kana,birth_dateが全て記入されていれば登録できる' do
+
+    context '新規登録がうまくいくとき' do
+      it 'nickname,email,password,passeord_confirmation,first_name,last_name,first_name_kana,last_name_kana,birth_dateが全て記入されていれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'passwordが６文字以上の半角英数字混合であれば登録できる' do
+        @user.password = "000aaa"
+        @user.password_confirmation = "000aaa"
+        expect(@user).to be_valid
+      end
+      it 'first_nameが全角であれば登録できる' do
+        @user.first_name = "山田"
+        expect(@user).to be_valid
+      end
+      it 'last_nameが全角であれば登録できる' do
+        @user.last_name = "太郎"
+        expect(@user).to be_valid
+      end
+      it 'first_name_kanaが全角であれば登録できる' do
+        @user.first_name_kana = "ヤマダ"
+        expect(@user).to be_valid
+      end
+      it 'last_name_kanaが全角であれば登録できる' do
+        @user.last_name_kana = "タロウ"
+        expect(@user).to be_valid
+      end
+    end 
+
+    context '新規登録がうまくいかないとき' do   
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
       
-    it 'nicknameが空では登録できない' do
+      it 'emaileが空では登録できない' do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
       
-    end
-    it 'emaileが空では登録できない' do
+      it 'emailが重複している場合は登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user,email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
       
-    end
-    it 'emailが重複している場合は登録できない' do
+      it 'passwordが空では登録できない' do
+        @user.password = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
       
-    end
-    it 'passwordが空では登録できない' do
+      it 'passeord_confirmationがpasswordと一致しなければ登録できない' do
+        @user.password_confirmation = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
       
-    end
-    it 'passeord_confirmationが空では登録できない' do
+      it 'passwordが6文字以上でなければ登録できない' do
+        @user.password = 12343
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
       
-    end
-    it 'passwordが6文字以上でなければ登録できない' do
+      it 'passwordが半角英数混合でなければ登録できない' do
+        @user.password = "eeeeあee"
+        @user.password_confirmation ="eeeeあee"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password は、半角英数字混合での入力が必須です")
+      end
       
-    end
-    it 'passwordが半角英数混合でなければ登録できない' do
+      it 'first_nameが空では登録できない' do
+        @user.first_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
       
-    end
-    it 'passwordとpasseord_confirmationが一致していなければ登録できない' do
+      it 'first_nameが全角でなければ登録できない' do
+        @user.first_name = "tarou"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name は、全角での入力が必須です")
+      end
       
-    end
-    it 'first_nameが空では登録できない' do
+      it 'last_nameが空では登録できない' do
+        @user.last_name = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
       
-    end
-    it 'first_nameが全角でなければ登録できない' do
+      it 'last_nameが全角でなければ登録できない' do
+        @user.last_name = "suzuki"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name は、全角での入力が必須です")
+      end
       
-    end
-    it 'last_nameが空では登録できない' do
+      it 'first_name_kanaが空では登録できない' do
+        @user.first_name_kana = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank")
+  
+      end
       
-    end
-    it 'last_nameが全角でなければ登録できない' do
+      it 'first_name_kanaが全角でなければ登録できない' do
+        @user.first_name_kana = "tarou"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana は、全角での入力が必須です")
+  
+      end
       
-    end
-    it 'first_name_kanaが空では登録できない' do
+      it 'last_name_kanaが空では登録できない' do
+        @user.last_name_kana = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+  
+      end
       
-    end
-    it 'first_name_kanaが全角でなければ登録できない' do
+      it 'last_name_kanaが全角でなければ登録できない' do
+        @user.last_name_kana = "suzuki"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana は、全角での入力が必須です")
+  
+      end
       
-    end
-    it 'last_name_kanaが空では登録できない' do
-      
-    end
-    it 'last_name_kanaが全角でなければ登録できない' do
-      
-    end
-    it 'birth_dateが空では登録できない' do
-      
+      it 'birth_dateが空では登録できない' do
+        #binding.pry
+        @user.birth_date = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
+      end
     end
 
   end
 end
 
 
-Input full-width characters
-Input full-width katakana characters
-Include both letters and numbers
