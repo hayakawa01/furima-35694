e1734d2f983642318e9f7eef04f08ee1bc750811
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-20 11:25:20 +0900

    ActiveHashの導入とバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Association::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :delivery_time
+  belongs_to :prefecture
+
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    with_options numericality: { other_than: 1 } do
+      validates :category
+      validates :condition
+      validates :delivery_charge
+      validates :prefecture
+      validates :delivery_time     
+    end
+    validates :price,numericality:{greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999},format:{with: /\A[0-9]+\z/,massage: "は、半角数字で入力してください"}
+  end
+
 end
