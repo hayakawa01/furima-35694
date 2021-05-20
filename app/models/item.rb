7ef0ef13b666d318c81584252069c3db4e195723
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-20 17:41:57 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,8 +11,8 @@ class Item < ApplicationRecord
 
   with_options presence: true do
     validates :image
-    validates :name
-    validates :detail
+    validates :name,length:{ maximum: 40}
+    validates :detail,length:{ maximum: 1000}
     with_options numericality: { other_than: 1 } do
       validates :category_id
       validates :condition_id
