461f1937392daaa1e6c4c439e88243d659d44c92
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-21 12:21:20 +0900

    商品詳細表示時の条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+    
+  end
+
   private
   def item_params
     params.require(:item).permit(:image,:name,:detail,:category_id,:condition_id,:delivery_charge_id,:prefecture_id,:delivery_time_id,:price).merge(user_id:current_user.id)
