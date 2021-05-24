381067b26c37437e60deb02960dba07989d51c55
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-24 21:18:01 +0900

    商品購入しているかどうかでの表示の記述

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -6,14 +6,14 @@ class OrderAddress
 
   with_options presence: true do
     validates :postal_code,format:{ with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is valid. Include hyphen(-)" }
+    validates :prefecture_id, numericality:{other_than: 1}
     validates :municipality
     validates :house_number
-    validates :phone_number,length:{maximum: 11}
+    validates :phone_number,length:{is: 11},format:{with: /\A[0-9]{11}\z/}
     validates :user_id
     validates :item_id
     validates :token
   end
-  validates :prefecture_id, numericality:{other_than: 1}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
