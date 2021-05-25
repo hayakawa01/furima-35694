2b8430c1ce3a18b093811ff618662c8b70b61f84
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-05-25 17:59:41 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -49,7 +49,7 @@ class ItemsController < ApplicationController
   end
 
   def correct_edit
-    unless current_user.id == @item.user_id
+    if current_user.id != @item.user_id || @item.order.present?
       redirect_to root_path
     end
   end
